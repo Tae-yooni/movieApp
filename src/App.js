d314634f5ccc10b4f584423f13b39bc0a3081ca5
Tae-yooni <duxodbs12@naver.com>
2018-08-08 15:41:52 +0900

    just right before state of React

--- src/App.js
+++ src/App.js
@@ -2,16 +2,32 @@ import React, { Component } from 'react';
 import './App.css';
 import Movie from './Movie';
 
+const movies = [
+  {
+    title: "Matrix",
+    poster: "https://m.media-amazon.com/images/M/MV5BNzQzOTk3OTAtNDQ0Zi00ZTVkLWI0MTEtMDllZjNkYzNjNTc4L2ltYWdlXkEyXkFqcGdeQXVyNjU0OTQ0OTY@._V1_SY1000_CR0,0,665,1000_AL_.jpg"
+  },
+  {
+    title: "Full Metal Jacket",
+    poster: "https://media.senscritique.com/media/000012416724/source_big/Full_Metal_Jacket.jpg"
+  },
+  {
+    title: "Oldboy",
+    poster: "https://m.media-amazon.com/images/M/MV5BMTk5NTEwMzI5OV5BMl5BanBnXkFtZTgwMDQ4ODUwMDE@._V1_SY1000_CR0,0,677,1000_AL_.jpg"
+  },
+  {
+    title: "Star Wars",
+    poster: "https://m.media-amazon.com/images/M/MV5BNzVlY2MwMjktM2E4OS00Y2Y3LWE3ZjctYzhkZGM3YzA1ZWM2XkEyXkFqcGdeQXVyNzkwMjQ5NzM@._V1_SY1000_CR0,0,643,1000_AL_.jpg"
+  }
+];
+
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
-        <Movie />
+        {movies.map(function(movie, index){
+          return <Movie title={movie.title} poster={movie.poster} key={index}/> 
+        })}
       </div>
     )
   }
--- src/Movie.js
+++ src/Movie.js
@@ -1,21 +1,35 @@
 import React, { Component } from 'react';
+import PropTypes from 'prop-types';
 import './Movie.css';
 
 class Movie extends Component {
+    
+    static propTypes = {
+        title: PropTypes.string.isRequired,
+        poster: PropTypes.string.isRequired
+    }
+    
+    
     render(){
         return (
             <div>
-                <MoviePoster />
-                <h1>hello this is a movie</h1>
+                <MoviePoster poster={this.props.poster} />
+                <h1>{this.props.title}</h1>
             </div>
         ) 
     }
 }
 
 class MoviePoster extends Component {
+
+    static propTypes = {
+        poster: PropTypes.string.isRequired
+    }
+
     render(){
+        // console.log(this.props);
         return(
-            <img src="http://4.bp.blogspot.com/-rP6iT-2Uero/VpXtLfs8CDI/AAAAAAAAKpk/KKj8n2KYfZ4/s400/%25EC%258B%259C%25EB%25B2%25A0%25EB%25A6%25AC%25EC%2595%2588%25ED%2597%2588%25EC%258A%25A4%25ED%2582%25A4%25EC%2595%2589%25EC%2595%2584%25EC%259E%2588%25EB%258A%2594%25EC%2582%25AC%25EC%25A7%2584.jpg"/>
+            <img src={this.props.poster}/>
         )
     }
 }
